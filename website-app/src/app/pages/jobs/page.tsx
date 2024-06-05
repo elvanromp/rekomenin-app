c3f48b4014fb7fce5c6d7330ac2bb20fc496972a
timotiustegar <tegar.timotius15@gmail.com>
2024-06-05 21:01:07 +0700

    add model

--- website-app/src/app/pages/jobs/page.tsx
+++ website-app/src/app/pages/jobs/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+"use client"
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-const jobs = () => {
+const MyComponent = () => {
+  const [recommendations, setRecommendations] = useState([]);
+
+  useEffect(() => {
+    const fetchRecommendations = async () => {
+      try {
+        const response = await axios.post('http://localhost:5000/predict', {user_id: 1779449,});
+        setRecommendations(response.data);
+        console.log(response.data)
+      } catch (error) {
+        console.error("Error fetching recommendations:", error);
+      }
+    };
+    fetchRecommendations();
+  }, []);
+
   return (
-    <div>jobs</div>
-  )
-}
+    <div>
+      test
+    </div>
+  );
+};
 
-export default jobs
+export default MyComponent;
